921468c7c5660c32df3400f36fdd949e62183317
Кузнецов Владислав Дмитриевич <kuznecovvd@clinic.local>
2024-10-14 11:21:35 +0800

    error CS0106

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,97 +1,88 @@
-﻿using System.Collections.Generic;
-using Model.Runtime.Projectiles;
-using UnityEngine;
-
-namespace UnitBrains.Player
-{
-    public class SecondUnitBrain : DefaultPlayerUnitBrain
+﻿    protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
     {
-        public override string TargetUnitName => "Cobra Commando";
-        private const float OverheatTemperature = 3f;
-        private const float OverheatCooldown = 2f;
-        private float _temperature = 0f;
-        private float _cooldownTime = 0f;
-        private bool _overheated;
-        private int _shotCount = 0;
+        float overheatTemperature = OverheatTemperature;
 
-        protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
+        int temp = GetTemperature();
+        if (temp >= overheatTemperature) { return; }
+        IncreaseTemperature();
+        for (int i = 0; i <= temp; i++)
         {
-            float overheatTemperature = OverheatTemperature;
-
-            int temp = GetTemperature();
-            if (temp >= overheatTemperature) { return; }
-            IncreaseTemperature();
-            for (int i = 0; i <= temp; i++)
-            {
-                var projectile = CreateProjectile(forTarget);
-                AddProjectileToList(projectile, intoList);//Переделал код под фидбек в надежде, что это спасет от багов которые могу я создать.
-
-            }
-
+            var projectile = CreateProjectile(forTarget);
+            AddProjectileToList(projectile, intoList);
         }
 
-        public override Vector2Int GetNextStep()
+    }
+
+    public override Vector2Int GetNextStep()
+    {
+    ///////////////////////////////////////
+    // Homework 1.4 (1st block, 4rd module)
+    ///////////////////////////////////////
+        List<Vector2Int> targets = SelectTargets();
+        if (targets.Count == 0 || IsTargetInRange(targets[0]))
         {
             return base.GetNextStep();
         }
-
-        protected override List<Vector2Int> SelectTargets()
+        else
         {
-            ///////////////////////////////////////
-            // Homework 1.4 (1st block, 4rd module)
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+            return CalcNextStepTowards(targets[0]);
+        }
+    }
+
+    protected override List<Vector2Int> SelectTargets()
+    {
+        List<Vector2Int> result = GetAllTargets();
+        if (result.Count == 0)
+        {
+            result.Add(GetEnemyBasePosition());
+        }
+        else
+        {
+            Vector2Int closestTarget = result[0];
+            float minDistance = DistanceToOwnBase(closestTarget);
+            for (int i = 1; i < result.Count; i++)
             {
-                result.RemoveAt(result.Count - 1);
-            }
-            Vector2Int? closestTarget = null;//*
-            float minDistance = float.MaxValue;
-            
-            foreach (Vector2Int target in result)
-            {
-                float distance = DistanceToOwnBase(target);
+                float distance = DistanceToOwnBase(result[i]);
                 if (distance < minDistance)
                 {
                     minDistance = distance;
-                    closestTarget = target;
+                    closestTarget = result[i];
                 }
             }
-
-            if (closestTarget != null)
+            if (!IsTargetInRange(closestTarget)) // цели не в атакующей зоне
             {
-                result.Clear();
-                result.Add(closestTarget.Value); // Использую .Value для получения значения из Nullable
-            }//*
-            return result;
-            ///////////////////////////////////////
-        }
-
-        public override void Update(float deltaTime, float time)
-        {
-            if (_overheated)
-            {              
-                _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
-                _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
-                if (t >= 1)
-                {
-                    _cooldownTime = 0;
-                    _overheated = false;
-                }
+                List<Vector2Int> outOfRangeTargets = new List<Vector2Int>();
+                outOfRangeTargets.Add(closestTarget); // Использую .Value для получения значения из Nullable
+                return outOfRangeTargets;
             }
         }
+        return result;
+    ///////////////////////////////////////
+    }
 
-        private int GetTemperature()
+public override void Update(float deltaTime, float time)
+{
+    if (_overheated)
+    {
+        _cooldownTime += Time.deltaTime;
+        float t = _cooldownTime / (OverheatCooldown / 10);
+        _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
+        if (t >= 1)
         {
-            if(_overheated) return (int) OverheatTemperature;
-            else return (int)_temperature;
-        }
-
-        private void IncreaseTemperature()
-        {
-            _temperature += 1f;
-            if (_temperature >= OverheatTemperature) _overheated = true;
+            _cooldownTime = 0;
+            _overheated = false;
         }
     }
+}
+
+protected int GetTemperature()
+{
+    if (_overheated) return (int)OverheatTemperature;
+    else return (int)_temperature;
+}
+
+protected void IncreaseTemperature()
+{
+    _temperature += 1f;
+    if (_temperature >= OverheatTemperature) _overheated = true;
 }
