41f728026da034fc7deae08f9f99a2650269d618
Кузнецов Владислав Дмитриевич <kuznecovvd@clinic.local>
2024-10-18 14:57:46 +0800

    не изменил Attack Delay с 0.75 до 0.15. и логику поведения(переделать)

---
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -0,0 +1,145 @@
+using System.Collections.Generic;
+using System.Linq;
+using Model;
+using Model.Runtime.Projectiles;
+using UnityEngine;
+using Utilities;
+
+namespace UnitBrains.Player
+{
+    public class ThirdUnitBrain : DefaultPlayerUnitBrain
+    {
+        public static int IdBilling = 0;
+        public int Id;
+
+        public override string TargetUnitName => "Ironclad Behemoth";
+
+        private const float OverheatTemperature = 3f;
+        private const float OverheatCooldown = 2f;
+        private const int MaxTarget = 3;
+
+        private float _temperature = 0f;
+        private float _cooldownTime = 0f;
+        private bool _overheated;
+
+        private Vector2Int _notRangeEnemyPosition;
+        private List<Vector2Int> allTargetEnemies = new List<Vector2Int>();
+
+        public ThirdUnitBrain()
+        {
+            Id = IdBilling++;
+        }
+
+        protected override List<Vector2Int> SelectTargets()
+        {
+            allTargetEnemies.Clear();
+            allTargetEnemies = GetAllTargets().ToList();
+
+            Vector2Int targetBaseEnemy = runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
+            allTargetEnemies.Remove(targetBaseEnemy);
+
+            // Если нужно оставить только одну цель
+            if (allTargetEnemies.Count > 0)
+            {
+                SortByDistanceToOwnBase(allTargetEnemies);
+
+                if (allTargetEnemies.Count > MaxTarget)
+                {
+                    allTargetEnemies = allTargetEnemies.GetRange(0, MaxTarget);
+                }
+
+                int idEnemy = (Id - 1) % allTargetEnemies.Count;
+                Vector2Int target = allTargetEnemies[idEnemy];
+                allTargetEnemies.Clear();
+
+                if (!HasTargetsInRange())
+                {
+                    _notRangeEnemyPosition = target;
+                }
+                else
+                {
+                    allTargetEnemies.Add(target);
+                }
+            }
+            else
+            {
+                allTargetEnemies.Clear();
+
+                if (!HasTargetsInRange())
+                {
+                    _notRangeEnemyPosition = targetBaseEnemy;
+                }
+                else
+                {
+
+                    allTargetEnemies.Add(targetBaseEnemy);
+                }
+            }
+            return allTargetEnemies;
+        }
+
+        public override Vector2Int GetNextStep()
+        {
+            // Возвращаем следующий шаг к цели
+            return unit.Pos.CalcNextStepTowards(_notRangeEnemyPosition);
+        }
+
+        public override void Update(float deltaTime, float time)
+        {
+            _stateTimer += deltaTime;
+
+            if (_overheated)
+            {
+                _cooldownTime += deltaTime;
+                float t = _cooldownTime / (OverheatCooldown / 10);
+                _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
+
+                if (t >= 1)
+                {
+                    _cooldownTime = 0;
+                    _overheated = false;
+                }
+                if (!CanShoot()) // Если не можем стрелять, движемся
+                {
+                    Move();
+                }
+                else // Если можем стрелять
+                {
+                    Shoot();
+                    IncreaseTemperature(); // Увеличиваем температуру после стрельбы
+                }
+            }
+
+            void Move()
+            {
+                Vector2Int nextStep = GetNextStep();
+                // Логика для движения к цели
+            }
+
+            void Shoot()
+            {
+                GenerateProjectiles(_notRangeEnemyPosition, new List<BaseProjectile>());
+            }
+
+            bool CanShoot()
+            {
+                return !(_overheated || !HasTargetsInRange());
+            }
+        }
+        
+
+        private int GetTemperature()
+        {
+            return _overheated ? (int)OverheatTemperature : (int)_temperature;
+        }
+
+        private void IncreaseTemperature()
+        {
+            _temperature += 1f;
+            if (_temperature >= OverheatTemperature)
+            {
+                _overheated = true;
+            }
+        }
+    }
+}
